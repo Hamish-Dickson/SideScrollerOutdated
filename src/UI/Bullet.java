c6bcfd9290e24e7b23f957cdb243d1294d3c95c2
Hamish Dickson <38864213+Hamish-Dickson@users.noreply.github.com>
2018-05-29 20:00:46 +0100

    commit 3.1
    fudged the last one, anyway

--- src/UI/Bullet.java
+++ src/UI/Bullet.java
@@ -7,24 +7,119 @@ import javafx.scene.shape.Circle;
 /**
  * Created by hdd00002 on 29/05/2018.
  */
-public class Bullet extends Circle{
+public class Bullet extends Circle {
     private double destinationX;
     private double destinationY;
+    private boolean moving;
+    boolean right;
+    boolean up;
+    private boolean vertical;
 
-
-    public Bullet(double startX, double startY, double destinationX, double destinationY){
-        super(startX,startY, 5, Color.RED);
-        this.destinationX = destinationX;
-        this.destinationY = destinationY;
-        correctTrajectory(destinationX, destinationY);
+    public boolean isVertical() {
+        return vertical;
     }
 
-    private void correctTrajectory(double destinationX, double destinationY) {
-        /*while(!(destinationX>=1280) || (destinationX<=0) || (destinationY>=720) || destinationY<=0){
-            this.destinationX+=10;
-            this.destinationY+=10;
-            destinationX+=10;
-            destinationY+=10;
+    public boolean isHorizontal() {
+        return horizontal;
+    }
+
+    private boolean horizontal;
+
+
+    public Bullet(double startX, double startY, double destinationX, double destinationY) {
+        super(startX, startY, 5, Color.BLACK);
+        this.destinationX = destinationX;
+        this.destinationY = destinationY;
+        this.moving = true;
+        setdirections();
+        correctTrajectory();
+    }
+
+    private void setdirections() {
+        if (destinationX > getCenterX()) {
+            right = true;
+        } else {
+            right = false;
+        }
+        if(destinationX == getCenterX()){
+            horizontal=true;
+        }
+        if(destinationY == getCenterY()){
+            vertical=true;
+        }
+        if (destinationY > getCenterY()) {
+            up = false;
+        } else {
+            up = true;
+        }
+    }
+
+    private void correctTrajectory() {
+        if(right){
+            if(destinationX<1280){
+                double a = destinationX-getCenterX();
+                double b = destinationY-getCenterY();
+                double c = Math.sqrt((a*a) + (b*b));
+                destinationY=destinationY*(destinationX/1280);
+                destinationX=1280;
+
+            }
+        }
+        /*if(right){
+            while(getDestinationX()<1280){
+                this.destinationX+=1;
+                if(up){
+                    this.destinationY+=-1;
+                }
+                else{
+                    this.destinationY+=1;
+                }
+            }
+        }
+        else{
+            while(getDestinationX()>0){
+                this.destinationX+= -1;
+                if(up){
+                    this.destinationY+=-1;
+                }
+                else{
+                    this.destinationY+=1;
+                }
+            }
+        }
+        if(up){
+            while(getDestinationY()>0){
+                this.destinationY+= -1;
+                if(right){
+                    this.destinationX+=1;
+                }
+                else{
+                    this.destinationX+=-1;
+                }
+            }
+        }
+        else{
+            while(getDestinationY()<720){
+                this.destinationY+= 1;
+                if(right){
+                    this.destinationX+=1;
+                }
+                else{
+                    this.destinationX+=-1;
+                }
+            }
+        }
+        /*while (!((destinationX >= 1280) || (destinationX <= 0) || (destinationY >= 720) || (destinationY <= 0))) {
+            if (this.destinationX < 360) {
+                destinationX += -1;
+            }
+            if (this.destinationY < 640) {
+                destinationY += -1;
+            } else {
+                //roughly keeps the bullets moving until the edge of screen on given trajectory
+                destinationX += 1;
+                destinationY += 1;
+            }
         }*/
     }
 
@@ -36,4 +131,19 @@ public class Bullet extends Circle{
         return destinationY;
     }
 
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
 }
--- src/UI/UIController.java
+++ src/UI/UIController.java
@@ -1,13 +1,14 @@
 package UI;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -49,26 +50,28 @@ public class UIController extends AnimationTimer {
         lblGameOver.setVisible(false);
         root.getChildren().add(lblGameOver);
         super.start();
-        root.setOnMouseClicked(mouseEvent -> {
+
+        root.setOnMousePressed(mouseEvent -> {
             //System.out.println(mouseEvent.getButton());
-            switch(mouseEvent.getButton()){
-                case SECONDARY: fire(mouseEvent);
+            switch (mouseEvent.getButton()) {
+                case SECONDARY:
+                    fire(mouseEvent);
             }
         });
         root.setOnKeyPressed(event -> {
             //System.out.println(event.getCode());
             switch (event.getCode()) {
                 case W:
-                    moveMeCircle.setCenterY(moveMeCircle.getCenterY() - deltaY*3);
+                    moveMeCircle.setCenterY(moveMeCircle.getCenterY() - deltaY * 3);
                     break;
                 case S:
-                    moveMeCircle.setCenterY(moveMeCircle.getCenterY() + deltaY*3);
+                    moveMeCircle.setCenterY(moveMeCircle.getCenterY() + deltaY * 3);
                     break;
                 case A:
-                    moveMeCircle.setCenterX(moveMeCircle.getCenterX() + deltaX*3);
+                    moveMeCircle.setCenterX(moveMeCircle.getCenterX() + deltaX * 3);
                     break;
                 case D:
-                    moveMeCircle.setCenterX(moveMeCircle.getCenterX() - deltaX*3);
+                    moveMeCircle.setCenterX(moveMeCircle.getCenterX() - deltaX * 3);
                     break;
                 case SPACE:
                     restartGame();
@@ -78,8 +81,15 @@ public class UIController extends AnimationTimer {
     }
 
     private void fire(MouseEvent mouseEvent) {
-        Bullet bullet = new Bullet(moveMeCircle.getCenterX(), moveMeCircle.getCenterY(),mouseEvent.getX(), mouseEvent.getY());
+        Bullet bullet = new Bullet(moveMeCircle.getCenterX(), moveMeCircle.getCenterY(), mouseEvent.getX(), mouseEvent.getY());
+        PathTransition transition = new PathTransition();
+        transition.setNode(bullet);
+        transition.setPath(new Line(moveMeCircle.getCenterX(),moveMeCircle.getCenterY(), bullet.getDestinationX(), bullet.getDestinationY()));
+        transition.setCycleCount(1);
+        transition.setDuration(Duration.seconds(3));
+        transition.play();
         bullets.add(bullet);
+
     }
 
     private void restartGame() {
@@ -106,7 +116,7 @@ public class UIController extends AnimationTimer {
                 }
                 fps = 0;
                 start = l;
-                spawnRate+=0.2;
+                spawnRate += 0.2;
             }
         }
         updateElements();
@@ -134,14 +144,28 @@ public class UIController extends AnimationTimer {
                 root.getChildren().remove(rectangle);
             }
         }
-        Random rand = new Random();
-        for(int i = 0; i<spawnRate; i++) {
+
+        for (Iterator<Bullet> iterator = bullets.listIterator(); iterator.hasNext(); ) {
+            Bullet bullet = iterator.next();
+            if (bullet.getCenterX() < 0 || bullet.getCenterX() > 1280 || bullet.getCenterY() < 0 || bullet.getCenterY() > 720) {
+                iterator.remove();
+                root.getChildren().remove(bullet);
+            }
+        }
+
+        Random rand;
+        for (int i = 0; i < spawnRate; i++) {
             rand = new Random();
             int posX = 1280;
             int posY = rand.nextInt(720);
-            rectangles.add(new Rectangle(posX, posY, 100, 50));
+            //rectangles.add(new Rectangle(posX, posY, 100, 50));
         }
 
+
+    }
+
+    private void updateElements() {
+        Random rand = new Random();
         for (Rectangle rect : rectangles) {
             if (!root.getChildren().contains(rect)) {
                 rect.setWidth(rand.nextInt(300 + 650));
@@ -149,23 +173,49 @@ public class UIController extends AnimationTimer {
             }
         }
 
-        for(Circle bullet: bullets) {
-            if(!root.getChildren().contains(bullet)){
+
+
+        for (Circle bullet : bullets) {
+            if (!root.getChildren().contains(bullet)) {
                 root.getChildren().add(bullet);
             }
         }
-    }
 
-    private void updateElements() {
         for (Rectangle rect : rectangles) {
             rect.setX(rect.getX() + deltaX);
         }
-        for(Bullet bullet: bullets){
-            System.out.println(bullet.getDestinationY());
-            System.out.println(bullet.getDestinationX());
-            bullet.setCenterX(bullet.getCenterX() + 0.01*(bullet.getDestinationX()-bullet.getCenterX()));
-            bullet.setCenterY(bullet.getCenterY()+ 0.01*(bullet.getDestinationY()-bullet.getCenterY()));
-            System.out.println(bullet.getCenterX() + bullet.getCenterY());
+        for (Bullet bullet : bullets) {
+            /*if (bullet.isMoving()) {
+                if(bullet.getCenterX()>bullet.getDestinationX()){
+                    bullet.setCenterX(bullet.getCenterX() - 0.1*(bullet.getDestinationX()/1280*100));
+                }else{
+                    bullet.setCenterX(bullet.getCenterX() + 0.1*(bullet.getDestinationX()/1280*100));
+                }
+                if (bullet.getCenterY()>bullet.getDestinationY()){
+                    bullet.setCenterY(bullet.getCenterY() - 0.1*(bullet.getDestinationY()/1280*100));
+                }
+                else{
+                    bullet.setCenterY(bullet.getCenterY() + 0.1*(bullet.getDestinationY()/1280*100));
+                }
+            }*/
+            /*if(bullet.isHorizontal()){
+                bullet.setTranslateX(bullet.getTranslateX()+bulletSpeed);
+            }
+            if(bullet.isVertical()){
+                bullet.setTranslateY(bullet.getTranslateY()+bulletSpeed);
+            }
+            if(bullet.isRight()){
+                bullet.setTranslateX(bullet.getTranslateX()+bulletSpeed+bullet.getDestinationX()/1280*100);
+            }
+            else{
+                bullet.setTranslateX(bullet.getTranslateX()-bulletSpeed-bullet.getDestinationX()/1280*100);
+            }
+            if(bullet.isUp()){
+                bullet.setTranslateY(bullet.getTranslateY()-bulletSpeed-bullet.getDestinationY()/720*100);
+            }
+            else {
+                bullet.setTranslateY(bullet.getTranslateY()+bulletSpeed+bullet.getDestinationY()/720*100);
+            }*/
         }
     }
 
